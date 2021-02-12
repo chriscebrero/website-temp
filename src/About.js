284c5d0c30827dd1e22de82ac9806c851007b619
Patrick <christiancebrero@Patricks-MacBook-Pro.local>
2021-02-11 16:20:43 -0800

    finished about

--- src/About.js
+++ src/About.js
@@ -3,7 +3,19 @@ import './about.css';
 function About() {
   return (
     <div className="about-container">
-        <p>ABOUT</p>
+        <div>
+            <h1>About Me</h1>
+        </div>
+        <div className="description">
+            <p>
+            I'm a 4th Year Combined Mathematics & Computer Science Student at UBC, with an interest in full-stack engineering, iOS app development and all things music production.
+
+When I'm not building apps and/or studying, you can find me playing the piano, playing Nintendo Switch, or tinkering with Logic Pro X.
+            </p>
+            <p>Check out my work experience and projects below.</p>
+        </div>
+
+
     </div>
   );
 }
